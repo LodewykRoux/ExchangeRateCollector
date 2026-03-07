69616e19ade22117ab0b37f2c43fa60fd71e63b0
Lodewyk Roux <RouxL2@ufs.ac.za>
2026-03-07 13:19:59 +0200

    Removed model

--- Models/ExchangeRateApi.cs
+++
@@ -1,9 +0,0 @@
-namespace ExchangeRateCollector.Models;
-
-public class ExchangeRateApi
-{
-    public string ExchangeRateApiUrl { get; set; }
-    public string ExchangeRateApiAccessKey { get; set; }
-    public string ExchangeRateApiBaseCurrency { get; set; }
-    public string ExchangeRateApiTargetCurrencies { get; set; }
-}
